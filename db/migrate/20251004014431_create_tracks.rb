ab25a3dcabcba30129c2783017c5607c80359cbd
Yorch <jorgelitox15@gmail.com>
2025-10-03 21:06:23 -0500

    Controllers and routes created

--- db/migrate/20251004014431_create_tracks.rb
+++ db/migrate/20251004014431_create_tracks.rb
@@ -1,6 +1,9 @@
 class CreateTracks < ActiveRecord::Migration[8.0]
   def change
     create_table :tracks do |t|
+      t.string :name
+      t.string :minutes
+      t.references :album
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_04_014431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "albums", force: :cascade do |t|
+    t.string "cover"
+    t.string "title"
+    t.string "artist"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "name"
+    t.string "minutes"
+    t.bigint "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["album_id"], name: "index_tracks_on_album_id"
+  end
 end
